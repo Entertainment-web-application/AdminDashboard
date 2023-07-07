ac37897ff9d0438b15b9a2d9a45d2f6305eba716
Farah <farahalmasri562@gmail.com>
2023-07-07 23:42:43 +0300

    edittt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,6 @@ router.post("/signup", validInfo, userController.signup);
 router.post("/login", validInfo, userController.login);
 
 // get user posts on users profile --FA
-router.get("/get-user-posts/:userId", userController.getUserPosts);
+// router.get("/get-user-posts/:userId", userController.getUserPosts);
 
 module.exports = router;
